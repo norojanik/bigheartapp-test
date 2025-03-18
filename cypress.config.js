1a1c9f145b540e88b6d381a743fe0a92c6234178
norbertjanik <janik.noro@gmail.com>
2025-03-18 15:25:25 +0100

    Update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://qa.bigheartapp.org/", // Change this to your app's URL
+    defaultCommandTimeout: 20000, // Set default timeout globally
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const file = config.env.configFile || "dev";
     },
   },
 });
